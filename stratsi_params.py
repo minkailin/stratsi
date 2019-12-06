regular update. enable fixed metallicity Z (calc dgmid numerically)

stratsi_params.py
@@ -11,6 +11,8 @@ from dedalus import public as de
 import h5py
 import argparse
 import time
+from scipy.integrate import quad
+from scipy.optimize import broyden1
 
 import logging
 logger = logging.getLogger(__name__)
@@ -30,8 +32,9 @@ eta_hat  = 0.05     #dimensionless radial pressure gradient
 '''
 dust parameters
 '''
-dg0      = 3.0      #midplane d/g ratio
-stokes   = 0.01    #assume a constant stokes number throughout 
+dg0      = 2.0     #midplane d/g ratio
+metal    = 0.02    #metallicity  
+stokes   = 1e-2    #assume a constant stokes number throughout 
 
 delta    = alpha*(1.0 + stokes + 4.0*stokes*stokes)/(1.0+stokes*stokes)**2
 beta     = (1.0/stokes - (1.0/stokes)*np.sqrt(1.0 - 4.0*stokes**2))/2.0
@@ -40,18 +43,19 @@ beta     = (1.0/stokes - (1.0/stokes)*np.sqrt(1.0 - 4.0*stokes**2))/2.0
 grid parameters
 '''
 zmin    = 0.0
-zmax    = 0.05
-nz_vert = 256 
+zmax    = 0.08
+nz_vert = 256
 
 '''
 mode parameters
 '''
 kx       = 400.0
-nz_waves = 64
+nz_waves = 128
     
 '''
 physics options 
 '''
+fix_metal     = True
 viscosity_eqm = False
 viscosity_pert= False
 diffusion     = True
@@ -61,19 +65,27 @@ backreaction  = True
 analytic vertical profiles for d/g, vdz, rhog assuming constant stokes number 
 '''
 def epsilon(z):
+    print("dg0=",dg0)
     return dg0*np.exp(-0.5*beta*z*z/delta)
 
 def rhog(z):
     return rhog0*np.exp( (delta/stokes)*(epsilon(z) - dg0) - 0.5*z*z)
 
-def epsilon_Z(z, dgmid):
-    return dgmid*np.exp(-0.5*beta*z*z/delta)
+def integrand_rhog(z, dg):
+    return np.exp( dg*(delta/stokes)*(np.exp(-0.5*beta*z*z/delta) - 1.0) - 0.5*z*z)
 
-def rhog_Z(z, dgmid):
-    return np.exp( (delta/stokes)*(epsilon_Z(z,dgmid) - dgmid) - 0.5*z*z)
+def integrand_rhod(z, dg):
+    rg  = integrand_rhog(z, dg)
+    eps = dg*np.exp(-0.5*beta*z*z/delta)
+    return eps*rg
 
-def rhod_Z(z, dgmid):
-    return epsilon(z, dgmid)*rhog_Z(z, dgmid)
+def sigma_g(dg):    
+    I = quad(integrand_rhog, 0.0, np.inf, args=(dg))
+    return I[0]
+
+def sigma_d(dg):
+    I = quad(integrand_rhod, 0.0, np.inf, args=(dg))    
+    return I[0]
 
 def vdz(z):
     return -beta*z
@@ -114,6 +126,21 @@ def d2ln_rhog(z):
 def dln_rhod(z):
     return dln_rhog(z) + dln_epsilon(z)
 
-def metallicity(dgmid):
-    
-    
+def metallicity_error(dg):
+    sigg = sigma_g(dg)
+    sigd = sigma_d(dg)
+    Z = sigd/sigg
+    return Z - metal
+
+def get_dg0_from_metal():
+    Hd      = np.sqrt(delta/(stokes+delta))
+    dgguess = metal/Hd
+    #print("dgguess=",dgguess)
+    sol     = broyden1(metallicity_error,[dgguess],f_tol=1e-16)
+    return sol[0]
+
+if fix_metal == True:
+    dg0 = get_dg0_from_metal()
+
+#print(epsilon(0.0))    
+#print("dg0=",dg0)