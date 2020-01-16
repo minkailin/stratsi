a3d5eb751ef5f40e1e324bb655766187a01e5c7a
Min-Kai Lin <mklin@asiaa.sinica.edu.tw>
2020-01-16 17:40:01 +0800

    fixed bug in 1fluid output

--- stratsi_plot.py
+++ stratsi_plot.py
@@ -5,12 +5,16 @@ import matplotlib.pyplot as plt
 import h5py
 import argparse
 
+from stratsi_params import delta, stokes
+
 '''
 process command line arguements
 '''
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", nargs='*', help="select mode number")
 parser.add_argument("--kx", nargs='*', help="select kx")
+parser.add_argument("--sig", nargs='*', help="select eigenvalue")
+
 args = parser.parse_args()
 if(args.mode):
     plot_mode = np.int(args.mode[0])
@@ -21,6 +25,12 @@ if(args.kx):
     plot_kx = np.float(args.kx[0])
 else:
     plot_kx = 400.0
+    
+if(args.sig):
+    eigenv = np.float(args.sig[0]) - np.float(args.sig[1])*1j
+else:
+    eigenv = 1.0
+
     
 #print("plotting mode number {0:3d}".format(plot_mode))
     
@@ -61,7 +71,10 @@ sigma_1f    = freqs_1f[n]
 growth_1f   = sigma_1f.real
 freq_1f     =-sigma_1f.imag
 
-g1          = np.argmax(growth_1f)
+if(args.sig):
+    g1 = np.argmin(np.abs(sigma_1f-eigenv))
+else:
+    g1          = np.argmax(growth_1f)
 
 W1f         = eig_W1f[n][g1]
 Q1f         = eig_Q1f[n][g1]
@@ -69,6 +82,7 @@ Ux          = eig_Ux[n][g1]
 Uy          = eig_Uy[n][g1]
 Uz          = eig_Uz[n][g1]
 
+del_rhod1f    = Q1f + W1f
 
 print("one-fluid model: kx, growth, freq = {0:1.2e} {1:13.6e} {2:13.6e}".format(kx_1f, growth_1f[g1], freq_1f[g1]))
 
@@ -119,7 +133,10 @@ sigma    = freqs[m]
 growth      = sigma.real
 freq        =-sigma.imag
 
-g1          = np.argmax(growth)
+if(args.sig):
+    g1 = np.argmin(np.abs(sigma-eigenv))
+else:
+    g1          = np.argmax(growth)
 
 W         = eig_W[m][g1]
 Q         = eig_Q[m][g1]
@@ -129,6 +146,8 @@ Ugz          = eig_Ugz[m][g1]
 Udx          = eig_Udx[m][g1]
 Udy          = eig_Udy[m][g1]
 Udz          = eig_Udz[m][g1]
+
+del_rhod    = Q + W
 
 print("two-fluid model: kx, growth, freq = {0:1.2e} {1:13.6e} {2:13.6e}".format(kx, growth[g1], freq[g1]))
 
@@ -152,7 +171,8 @@ plot eigenvalues
 plt.rc('font',size=fontsize,weight='bold')
 
 fig, axs = plt.subplots(2, sharex=True, sharey=False, gridspec_kw={'hspace': 0.1}, figsize=(8,6))
-plt.subplots_adjust(left=0.18, right=0.95, top=0.95, bottom=0.15)
+#plt.subplots_adjust(left=0.18, right=0.95, top=0.95, bottom=0.15)
+plt.subplots_adjust(left=0.16, right=0.95, top=0.95, bottom=0.15)
 #plt.suptitle(title,y=0.99,fontsize=fontsize,fontweight='bold')
 plt.xscale('log')
 
@@ -172,7 +192,7 @@ for i, k in enumerate(ks):
 
 axs[0].set_ylabel(r'$s/\Omega$')
 lines1, labels1 = axs[0].get_legend_handles_labels()
-legend=axs[0].legend(lines1, labels1, loc='upper left', frameon=False, ncol=1, handletextpad=-0.5)
+legend=axs[0].legend(lines1, labels1, loc='upper left', frameon=False, ncol=1, handletextpad=-0.5,fontsize=fontsize/2)
 
 for i, k in enumerate(ks_1f):
     for n, sig in enumerate(freqs_1f[i]):
@@ -193,12 +213,41 @@ for i, k in enumerate(ks):
 axs[1].set_ylabel(r'$\omega/\Omega$')
 axs[1].set_xlabel(r'$k_xH_g$')
 lines1, labels1 = axs[1].get_legend_handles_labels()
-legend=axs[1].legend(lines1, labels1, loc='upper left', frameon=False, ncol=1, handletextpad=-0.5)
+legend=axs[1].legend(lines1, labels1, loc='upper left', frameon=False, ncol=1, handletextpad=-0.5, fontsize=fontsize/2)
 
 plt.xlim(np.amin(ks),np.amax(ks))
 
 fname = 'stratsi_plot_growth'
 plt.savefig(fname,dpi=150)
+
+'''
+plot eigenvalues as scatter diagram for a single kx 
+'''
+
+fig = plt.figure(figsize=(8,4.5))
+ax = fig.add_subplot()
+plt.subplots_adjust(left=0.2, right=0.95, top=0.9, bottom=0.2)
+
+title=r"$K_x$={0:4.0f}, St={1:4.0e}, $\delta$={2:4.0e}".format(ks[m], stokes, delta)
+plt.suptitle(title,y=0.99,fontsize=fontsize,fontweight='bold',x=0.55)
+
+plt.scatter(-sigma_1f.imag, sigma_1f.real, marker='o', label=r'one fluid',color='black',s=64)
+plt.scatter(-sigma.imag, sigma.real, marker='X', label=r'two fluid',color='red',s=64)
+
+plt.rc('font',size=fontsize,weight='bold')
+
+lines1, labels1 = ax.get_legend_handles_labels()
+legend=ax.legend(lines1, labels1, loc='upper right', frameon=False, ncol=1, handletextpad=-0.5, fontsize=fontsize/2)
+
+plt.xticks(fontsize=fontsize,weight='bold')
+plt.xlabel(r'$\omega/\Omega$',fontsize=fontsize)
+
+plt.yticks(fontsize=fontsize,weight='bold')
+plt.ylabel(r'$s/\Omega$', fontsize=fontsize)
+
+fname = 'stratsi_plot_eigen'
+plt.savefig(fname,dpi=150)
+
 
 '''
 plot eigenfunctions
@@ -276,18 +325,24 @@ plt.savefig(fname,dpi=150)
 
 fig = plt.figure(figsize=(8,4.5))
 ax = fig.add_subplot()
-plt.subplots_adjust(left=0.18, right=0.95, top=0.95, bottom=0.2)
+plt.subplots_adjust(left=0.18, right=0.95, top=0.9, bottom=0.2)
+
+title=r"$K_x$={0:4.0f}, St={1:4.0e}, $\delta$={2:4.0e}".format(ks[m], stokes, delta)
+plt.suptitle(title,y=0.99,fontsize=fontsize,fontweight='bold',x=0.55)
 
 plt.xlim(xmin,xmax)
 plt.ylim(0,ymax)
 
-Q1f_abs = np.abs(Q1f)
-Q1f_abs/= np.amax(Q1f_abs)
-plt.plot(z_1f, Q1f_abs, linewidth=2, label=r'one-fluid')
+dpert_1f = np.abs(del_rhod1f)
+dpert_1f/= np.amax(dpert_1f)
+plt.plot(z_1f, dpert_1f, linewidth=2, label=r'one-fluid',color='black')
 
-Q_abs = np.abs(Q)
-Q_abs/= np.amax(Q_abs)
-plt.plot(z, Q_abs, linewidth=2, label=r'two-fluid',linestyle='dashed')
+dpert = np.abs(del_rhod)
+dpert/= np.amax(dpert)
+plt.plot(z, dpert, linewidth=2, label=r'two-fluid',linestyle='dashed',color='r')
+
+#plt.plot(z, eig_W[m][g1].real, linewidth=2, label=r'one-fluid',color='black')
+#plt.plot(z, eig_W[m][g1+1].real, linewidth=2, label=r'two-fluid',linestyle='dashed',color='r')
 
 plt.rc('font',size=fontsize,weight='bold')
 
@@ -298,7 +353,7 @@ plt.xticks(fontsize=fontsize,weight='bold')
 plt.xlabel(r'$z/H_g$',fontsize=fontsize)
 
 plt.yticks(fontsize=fontsize,weight='bold')
-plt.ylabel(r'$|\delta\epsilon/\epsilon|$', fontsize=fontsize)
+plt.ylabel(r'$|\delta\rho_d/\rho_d|$', fontsize=fontsize)
 
 fname = 'stratsi_plot_Q'
 plt.savefig(fname,dpi=150)
